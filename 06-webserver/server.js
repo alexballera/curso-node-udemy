4304fad866f884cb9a5f4417acc7c687661f1ed9
Alex Ballera <alexballera@gmail.com>
2020-07-02 13:31:40 -0300

    Usando helpers

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,27 +1,28 @@
 const express = require('express')
 const app = express()
+
 const hbs = require('hbs')
+
+require('./hbs/helpers')
 
 app.use(express.static(__dirname + '/public'))
 
 // Express HBS engine
-
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
+
+
  
 app.get('/', (req, res) => {
   
   res.render('home', {
-    name: 'Alexander',
-    year: new Date().getFullYear()
+    name: 'alexander',
   })
 })
 
 app.get('/about', (req, res) => {
   
-  res.render('about', {
-    year: new Date().getFullYear()
-  })
+  res.render('about')
 })
  
 app.listen(3000, () => console.log('Escuchando peticiones en puerto 3000'))
