518b3b50db3bf8189c0ae3ffddbb9c3f8ed3f968
Alex Ballera <alexballera@gmail.com>
2020-07-01 22:58:54 -0300

    Contenido estatico

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', (req, res) => {
-  
-  let salida = {
-    nombre: 'Alexander',
-    edad: 40,
-    url: req.url
-  }
-  
-  res.send(salida)
-})
 
-app.get('/data', (req, res) => {
-  res.send('Hola Data')
-})
+app.use(express.static(__dirname + '/public'))
+ 
+// app.get('/', (req, res) => {
+  
+//   let salida = {
+//     nombre: 'Alexander',
+//     edad: 40,
+//     url: req.url
+//   }
+  
+//   res.send(salida)
+// })
+
+// app.get('/data', (req, res) => {
+//   res.send('Hola Data')
+// })
  
 app.listen(3000, () => console.log('Escuchando peticiones en puerto 3000'))
