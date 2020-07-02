d09501d0872965e9a598d9426b5ca64d6df36582
Alex Ballera <alexballera@gmail.com>
2020-07-02 09:27:09 -0300

    Usando parciales

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
 
 app.use(express.static(__dirname + '/public'))
 
 // Express HBS engine
+
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
  
 app.get('/', (req, res) => {
   
   res.render('home', {
     name: 'Alexander',
+    year: new Date().getFullYear()
+  })
+})
+
+app.get('/about', (req, res) => {
+  
+  res.render('about', {
     year: new Date().getFullYear()
   })
 })
