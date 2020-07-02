92f61c8f6033a10aa6d4f8ee8eb4a7f968f2b7db
Alex Ballera <alexballera@gmail.com>
2020-07-01 22:53:27 -0300

    Introduccion express

---
+++ 06-webserver/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/', (req, res) => {
+  
+  let salida = {
+    nombre: 'Alexander',
+    edad: 40,
+    url: req.url
+  }
+  
+  res.send(salida)
+})
+
+app.get('/data', (req, res) => {
+  res.send('Hola Data')
+})
+ 
+app.listen(3000, () => console.log('Escuchando peticiones en puerto 3000'))
