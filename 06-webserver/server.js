83e54e8f889d6571357c60489ddf5f34d25bec0c
Alex Ballera <alexballera@gmail.com>
2020-08-03 09:11:16 -0300

    Configuracion port

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-
 const hbs = require('hbs')
-
 require('./hbs/helpers')
 
+const port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
 
 // Express HBS engine
@@ -25,4 +24,4 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
  
-app.listen(3000, () => console.log('Escuchando peticiones en puerto 3000'))
+app.listen(port, () => console.log(`Escuchando peticiones en puerto ${port}`))
