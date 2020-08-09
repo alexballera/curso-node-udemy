dd179632743442302959fcec1b247814b857332c
Alex Ballera <alexballera@gmail.com>
2020-08-08 22:20:22 -0300

    Creado crud

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
 const port = 3000
 
-app.get('/', function(req, res) {
-  res.json('hello world');
+app.get('/usuario', function(req, res) {
+  res.json('Get Usuario');
+});
+
+app.post('/usuario', function(req, res) {
+
+  let body = req.body
+  res.json({
+    persona: body
+  });
+});
+
+app.put('/usuario/:id', function(req, res) {
+  let id = req.params.id
+  res.json({
+    id
+  });
+});
+
+app.delete('/usuario/:id', function(req, res) {
+  let id = req.params.id
+  res.json({
+    id
+  });
 });
 
 app.listen(port, () => console.log('Escuchando puerto', port))
